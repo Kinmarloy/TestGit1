7eaf71bf2da671218ad204b94e5231942e900a6c
Kinmarloy <kinmarloy@yadex.ru>
2022-10-20 20:49:04 +0300

    added new class

---
+++ src/test/java/GitTestMerge/GitTestMergeClass1.java
@@ -0,0 +1,12 @@
+package GitTestMerge;
+
+public class GitTestMergeClass1 {
+    public static void main(String[] args) {
+
+        int a = 1;
+        String b = "234";
+
+        System.out.println( a + b );
+
+    }
+}
