acd44814034cfaee776d18a7b095db0d69587b23
Kinmarloy <kinmarloy@yadex.ru>
2022-10-20 21:07:37 +0300

    added changes

--- src/test/java/GitTestMerge/GitTestMergeClass1.java
+++ src/test/java/GitTestMerge/GitTestMergeClass1.java
@@ -4,10 +4,13 @@ public class GitTestMergeClass1 {
     public static void main(String[] args) {
 
         int a = 1;
+        int n = 2;
         int x = 3;
         String b = "234";
 
+        System.out.println( a + b + n);
         System.out.println( a + b + x);
+
 
     }
 }
